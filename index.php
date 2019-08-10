91fabc7d96210bf680a25402b9ae8661c973528b
Dinesh Tripathi <dinesh.tripathi30@gmail.com>
2019-08-10 23:45:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
             <li><a href="#">Home</a></li>
             <li>Basic</li>
           </ol>
-          <h1>Welcome to ICDS OpenShift Container Platform!</h1>
+          <h1>Welcome to VMworld 2019 !</h1>
           <p>This is ICDS Offering OpenShift Container Platform flavour demo. We hope you enjoyed it!</p>
         </div>
       </div>
